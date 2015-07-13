c48d08c8ec18087757086757a3cf26201516bced
Bnizzle <brennan.chan91@gmail.com>
2015-07-13 22:31:59 +0800

    Created the models.
    Added some validation
    Added some fixtures
    Created the migrations and made a change to them once they were commited to the db

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  belongs_to :company
+  has_many :works
+  has_many :projects, :through => :works
+
+  validates :fname, length: { minimum: 2 }
+  validates :lanme, length: { minimum: 5 }
+  validates :company_id, presence: true
+end
---
+++ app/models/work.rb
@@ -0,0 +1,19 @@
+class Work < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :project
+
+  validates :project_id, presence: true
+  validates :user, presence: true
+  validates :datetimeperformed, presence: true
+  validate :date_is_in_past
+  validates :hours, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 8 }
+
+  scope :fullday, -> { where("hours >= 8") }
+  scope :recent, -> { where("datetimeperformed > '#{Time.now - 7.days}'")}
+
+  def date_is_in_past
+    if datetimeperformed.present? && datetimeperformed > Time.now
+      errors.add(:datetimeperformed, "can't be in the future")
+    end
+  end
+end
