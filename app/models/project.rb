622bf4d886d05c4ae96a42f6a0426ec79e5f3a80
Bnizzle <brennan.chan91@gmail.com>
2015-07-16 21:48:49 +0800

    Added Slug fields

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,7 @@ class Project < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 5 }
   validates :company_id, presence: true
   validates :default_rate, numericality: { only_integer: true, greater_than_or_equal_to: 50, less_than: 10000 }
+  validates :slug, uniqueness: true, length: { minimum: 3 }
 
   scope :lowdefaultrate, -> { where("default_rate < 100")}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150713133713) do
+ActiveRecord::Schema.define(version: 20150716132527) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150713133713) do
     t.integer  "default_rate", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "slug",         limit: 255
   end
 
   create_table "users", force: :cascade do |t|
